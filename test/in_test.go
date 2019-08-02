d1df9949a6ce6fbdbc5d12e0b0ef657ae79cc6c5
whr <whr@whr.local>
2019-08-02 10:54:47 +0800

    a

---
+++ test/in_test.go
@@ -0,0 +1,47 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+func In(a interface{}, l ...interface{}) bool {
+	if l == nil || len(l) == 0 {
+		return false
+	}
+
+	for _, v := range l {
+		if a == v {
+			return true
+		}
+	}
+
+	return false
+}
+
+
+func Test_in(t *testing.T) {
+	if In(7,2,int64(3),int64(5),7){
+		fmt.Println(" true")
+	}
+	fmt.Println(" end ")
+}
+
+func Test_f(t *testing.T) {
+
+	f := float64(500.012345678)
+	fmt.Print(fmt.Sprintf("%.2f",f))
+
+}
+
+func Test_strings_replacef(t *testing.T) {
+	msg := "上分500000"
+	src := "上分"
+	s := strings.Replace(msg, src, "",-1)
+	fmt.Print(fmt.Println("->",s))
+}
+
+
+
+
+
---
+++ test/json_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Msg struct{
+	Type int64
+	Content interface{}
+}
+type ContentTest struct{
+	ID int64
+	Text string
+}
+
+
+func Test_json_unmashal(t *testing.T) {
+	msg := &Msg{
+		Type: 1,
+		Content: &ContentTest{
+			ID:   1,
+			Text: "333333,,sss",
+		},
+	}
+
+	s,_ := json.Marshal(msg)
+	fmt.Println("s:",string(s))
+	fmt.Println("-----------------")
+	//-------- -----------------------------
+	msg2 := new(Msg)
+	json.Unmarshal(s,msg2)
+
+	fmt.Println(fmt.Sprintf("msg2 %v",*msg2))
+	fmt.Println(fmt.Sprintf("msg2 %+v",*msg2))
+	fmt.Println(fmt.Sprintf("msg2 %#v",*msg2))
+}
+
+
