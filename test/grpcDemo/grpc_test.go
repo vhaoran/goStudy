559998216f3b7b3b75effb7d688f6d5e7adf9623
whr <whr@whr.local>
2019-12-01 20:22:24 +0800

    a

--- test/grpcDemo/grpc_test.go
+++ test/grpcDemo/grpc_test.go
@@ -3,19 +3,20 @@ package grpcDemo
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/golang/protobuf/proto"
 	"testing"
 	"time"
+
+	"github.com/golang/protobuf/proto"
 )
 
 type (
 	Person struct {
-		ID   string `protobuf:"bytes,1,opt,name=id" json:"id,omitempty"`
+		ID   int32  `protobuf:"varint,1,opt,name=id" json:"id,omitempty"`
 		Name string `protobuf:"bytes,2,opt,name=name" json:"name,omitempty"`
 	}
 )
 
-var high = 100000
+var high = int32(1000000)
 
 func (p *Person) Reset() {
 	p = &Person{}
@@ -29,50 +30,55 @@ func (p Person) ProtoMessage() {}
 
 func Test_proto_buffer(t *testing.T) {
 	t0 := time.Now()
-	for i := 0; i < high; i++ {
+	ii := 0
+	for i := int32(0); i < high; i++ {
 		bean := &Person{
-			ID:   fmt.Sprint(i),
+			ID:   i,
 			Name: fmt.Sprint("aa", i),
 		}
 		if buffer, err := proto.Marshal(bean); err == nil {
+			ii = len(buffer)
+
 			p := new(Person)
 			proto.Unmarshal(buffer, p)
+			//fmt.Println("len: ", len(buffer))
 		} else {
 			fmt.Println("error:", err)
 		}
 	}
-	fmt.Println("all time:", time.Since(t0))
+	fmt.Println("all time:", time.Since(t0), " ", ii)
 }
 
 func Test_json(t *testing.T) {
 	t0 := time.Now()
-	for i := 0; i < high; i++ {
+	ii := 0
+	for i := int32(0); i < high; i++ {
 		bean := &Person{
-			ID:   fmt.Sprint(i),
+			ID:   i,
 			Name: fmt.Sprint("aa", i),
 		}
 		if buffer, err := json.Marshal(bean); err == nil {
+			ii = len(buffer)
 			p := new(Person)
 			json.Unmarshal(buffer, p)
 		} else {
 			fmt.Println("error:", err)
 		}
 	}
-	fmt.Println("all time:", time.Since(t0))
+	fmt.Println("all time:", time.Since(t0), "  ", ii)
 
 }
 
 func Test_empty_noAction(t *testing.T) {
 	t0 := time.Now()
-	j := ""
-	for i := 0; i < high; i++ {
+	j := int32(0)
+	for i := int32(0); i < high; i++ {
 		bean := &Person{
-			ID:   fmt.Sprint(i),
+			ID:   i,
 			Name: fmt.Sprint("aa", i),
 		}
 		j = bean.ID
 	}
 
 	fmt.Println("all time:", time.Since(t0), " // ", j)
-
 }
