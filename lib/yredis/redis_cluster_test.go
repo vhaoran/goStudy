5e4d44b82a4fc3934960b97ade42e7ec2285a806
whr <whr@whr.local>
2019-12-05 18:15:16 +0800

    a

--- lib/yredis/redis_cluster_test.go
+++ lib/yredis/redis_cluster_test.go
@@ -3,20 +3,18 @@ package yredis
 import (
 	"fmt"
 	"log"
-	"sync"
 	"testing"
 	"time"
 )
 
 func Test_redis(t *testing.T) {
-	host := "127.0.0.1"
 	url := []string{
-		fmt.Sprintf("%s:7001", host),
-		fmt.Sprintf("%s:7002", host),
-		fmt.Sprintf("%s:7003", host),
-		fmt.Sprintf("%s:7004", host),
-		fmt.Sprintf("%s:7005", host),
-		fmt.Sprintf("%s:7006", host),
+		"192.168.0.99:7001",
+		"192.168.0.99:7002",
+		"192.168.0.99:7003",
+		"192.168.0.99:7004",
+		"192.168.0.99:7005",
+		"192.168.0.99:7006",
 	}
 
 	red, err := NewRedisClient(url)
@@ -26,23 +24,18 @@ func Test_redis(t *testing.T) {
 	}
 
 	//
-	h := 10000
-	var wg sync.WaitGroup
-	wg.Add(h)
 	t0 := time.Now()
-	for i := 0; i < h; i++ {
-		go func(k int) {
-			if err := red.Set(fmt.Sprint("a_", k), k, time.Hour*100).Err(); err != nil {
-				log.Println(err)
-			} else {
-				if i%100 == 0 {
-					//log.Println("OK", i)
-				}
+	for i := 0; i < 10000; i++ {
+		if err := red.Set(fmt.Sprint("a_", i), i, time.Hour*100).Err(); err != nil {
+			log.Println(err)
+		} else {
+			if i%100 == 0 {
+				log.Println("OK", i)
 			}
-			wg.Done()
-		}(i)
+		}
 	}
 	//
-	wg.Wait()
-	fmt.Println("---------aaa---count--", h, " time:", time.Since(t0))
+
+	//
+	fmt.Println("---------aaa-----", time.Since(t0))
 }
